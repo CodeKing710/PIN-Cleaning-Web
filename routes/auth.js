006be0e694467691b25f60f7994e0d20623915cb
Carter Ervin <cjrox01@yahoo.com>
2022-06-15 21:28:32 -0500

    Desktop CSS work, reorganized setup of page to use PLACSS wrapper preset, begin login

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,27 @@ const auth = require("express").Router();
 
 //Routes
 auth.get('/', (req,res) => {
-  res.write("<h1>This shouldn't have a view but will post for when users sign-up or login!</h1>")
+  res.write("<h1>This shouldn't have a view but will post for when users sign-up or login!</h1>");
+});
+
+auth.get('/signup', async (req,res) => {
+  if(!req.session.userid) {
+    res.render('user/signup');
+  } else {
+    res.redirect('/');
+  }
+});
+
+auth.get('/login', async (req,res) => {
+  if(!req.session.userid) {
+    res.render('user/login');
+  } else {
+    res.redirect('/');
+  }
+});
+
+auth.post('/', async (req,res) => {
+
 });
 
 module.exports = auth;
