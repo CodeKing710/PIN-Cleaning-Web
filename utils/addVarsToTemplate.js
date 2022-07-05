bb9d78550be96fb2852f33081bce18b3acfa7c9c
Carter Ervin <cjrox01@yahoo.com>
2022-07-05 11:19:32 -0500

    minification

--- utils/addVarsToTemplate.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (vars) {
-  return function (req, res, next) {
-    if(typeof vars === 'object') {
-      req.app.locals = { ...(req.app.locals), ...vars}
-    }
-    req.app.locals.userid = req.session?.userid ?? 'guest';
-    next();
-  };
-};
--- utils/index.js
+++
@@ -1,2 +0,0 @@
-exports.addVarsToTemplate = require("./addVarsToTemplate");
-// exports.sessions = require('./session');
