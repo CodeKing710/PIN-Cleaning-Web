eeea2125261b5040de18afc83f747bfb92d4e7d4
Carter Ervin <cjrox01@yahoo.com>
2022-07-07 13:43:24 -0500

    removed secondary server

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
-const ssl = express();
+// const ssl = express();
 const PORT = process.env.PORT ?? 3000;
-const S_PORT = process.env.S_PORT ?? 403;
+// const S_PORT = process.env.S_PORT ?? 403;
 const routes = require('./routes');
 const fs = require('fs');
 global.cache = {};
@@ -27,17 +27,17 @@ app.get('*', (req, res) => {
   res.status(404).render('404');
 });
 
-const ssl_routes = require('./routes/transactions');
-ssl.use(express.json());
-ssl.use(express.urlencoded({extended: true}));
+// const ssl_routes = require('./routes/transactions');
+// ssl.use(express.json());
+// ssl.use(express.urlencoded({extended: true}));
 // ssl.use(fs.readFileSync('./certs/key.pem'));
 
-for(const route in ssl_routes) {
-  ssl.use(ssl_routes[route].path, ssl_routes[route].ctrl);
-}
+// for(const route in ssl_routes) {
+//   ssl.use(ssl_routes[route].path, ssl_routes[route].ctrl);
+// }
 
 app.listen(PORT, console.log(`Listening on port ${PORT}...`));
-require('https').createServer({
-  key: process.env.SSLKEY,
-  cert: process.env.SSLCERT
-}, ssl).listen(S_PORT, console.log(`SSL Secure Server Listening on ${S_PORT}...`));
+// require('https').createServer({
+//   key: process.env.SSLKEY,
+//   cert: process.env.SSLCERT
+// }, ssl).listen(S_PORT, console.log(`SSL Secure Server Listening on ${S_PORT}...`));
