5e3b2b966519dad9790352e55b07afb969406e3b
Carter Ervin <cjrox01@yahoo.com>
2022-10-17 10:21:34 -0500

    added email receipts

--- routes/payment/main.js
+++ routes/payment/main.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const payment = require('express').Router();
 const {User} = require('../../models');
-const stripe = require('stripe')(process.env.SAPI_KEY);
+const stripe = require('stripe')(process.env.TSAPI_KEY);
 
 payment.get('/', async (req,res) => {
   try {
@@ -42,6 +42,32 @@ payment.post('/', async (req,res) => {
     } catch (e) {
       console.log("Cart Empty Unsuccessful: "+e);
     }
+
+    //Send Receipt
+    const mailer = require('nodemailer').createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'admin@thepriceisnicecleaning.com',
+        pass: 'z3axtM37!'
+      }
+    });
+
+    const user = await User.findOne({username: req.session.userid});
+
+    const mailOptions = {
+      from: 'admin@thepriceisnicecleaning.com',
+      to: user.email,
+      subject: `Order for ${user.username}`,
+      text: desc
+    };
+
+    mailer.sendMail(mailOptions, (e, info) => {
+      if(e){
+        console.log(e);
+      } else {
+        console.log("Email sent: "+info.response);
+      }
+    });
 
     res.render('cart/success');
   } catch (e) {
